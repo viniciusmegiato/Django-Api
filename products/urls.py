ff54df0a18da595a8c9c02f916f0bd802b9cfb7e
viniciusmegiato <viniciusmegiato@outlook.com>
2024-04-22 19:09:50 -0300

    fix: ajustes path requisições

--- products/urls.py
+++ products/urls.py
@@ -9,9 +9,7 @@ router.register(r'products', ProductsViewSet, basename='product')
 urlpatterns = [
     # path('', views.ProductsViewSet.as_view({'get': 'list', 'post': 'create'}), name='products-list'),
     path('products/', ProductsViewSet.as_view({'get': 'list', 'post': 'create'}), name='products-list'),
-    path('products/<int:pk>/', ProductsViewSet.as_view({'get': 'retrieve'}), name='product-detail'),
-    path('products/update/<int:pk>/', ProductsViewSet.as_view({'get': 'retrieve', 'put': 'update'}), name='product-update'),
-    path('products/delete/<int:pk>/', ProductsViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name='product-delete'),
+    path('products/<int:pk>/', ProductsViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='product-detail'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
