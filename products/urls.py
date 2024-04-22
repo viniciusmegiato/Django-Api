bc013e7dab77b4d42ac4beb8699e074d9d4b5a9e
viniciusmegiato <viniciusmegiato@outlook.com>
2024-04-22 19:19:13 -0300

    feat: add url pagina inicial e remoção de imports desnecessários

--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path, include, re_path
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import ProductsViewSet
+from .views import ProductsViewSet, HomeView
 
 router = DefaultRouter()
 router.register(r'products', ProductsViewSet, basename='product')
 
 urlpatterns = [
-    # path('', views.ProductsViewSet.as_view({'get': 'list', 'post': 'create'}), name='products-list'),
+    path('', HomeView.as_view(), name='home'),
     path('products/', ProductsViewSet.as_view({'get': 'list', 'post': 'create'}), name='products-list'),
     path('products/<int:pk>/', ProductsViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='product-detail'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
